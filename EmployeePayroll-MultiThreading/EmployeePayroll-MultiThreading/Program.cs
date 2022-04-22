7077808eb95482a4e8c0f891c82753bd1fed04c4
Ankush Bahekar <ankushmb9@gmail.com>
2022-04-22 14:17:15 +0530

    [Ankush] Add: Ability to added multiple employee to payroll DB

---
+++ EmployeePayroll-MultiThreading/EmployeePayroll-MultiThreading/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll_MultiThreading
+{
+    public class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Welcome to Employee Payroll Database");
+            EmployeePayrollDatabase employeePayrollDatabase = new EmployeePayrollDatabase();
+            int UC = Convert.ToInt32(Console.ReadLine());
+            switch (UC)
+            {
+
+            }
+        }
+    }
+}
